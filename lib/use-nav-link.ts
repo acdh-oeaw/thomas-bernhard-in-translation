027ed2dc47b04eef421ea3653ab31dc1aa53bbc1
Kevin Stadler <kevin.stadler@oeaw.ac.at>
2024-08-19 14:21:01 +0200

    fix: make AppNavLink sensitive to search params

--- lib/use-nav-link.ts
+++ lib/use-nav-link.ts
@@ -1,3 +1,5 @@
+import { useSearchParams } from "next/navigation";
+
 import type { LinkProps } from "@/components/link";
 import { env } from "@/config/env.config";
 import { createFullUrl } from "@/lib/create-full-url";
@@ -11,6 +13,7 @@ export function useNavLink(params: UseNavLinkParams): UseNavLinkReturnValue {
 	const { "aria-current": ariaCurrent, href } = params;
 
 	const pathname = usePathname();
+	const searchParams = useSearchParams();
 
 	if (ariaCurrent != null) {
 		return {
@@ -19,7 +22,21 @@ export function useNavLink(params: UseNavLinkParams): UseNavLinkReturnValue {
 	}
 
 	const url = createFullUrl({ pathname: href });
-	const isCurrent = url.origin === env.NEXT_PUBLIC_APP_BASE_URL && url.pathname === pathname;
+
+	// FIXME when a link href only has new search params (e.g. href="?x=y") the url.pathname is "/",
+	// even though the actual target of the nav-link is whatever the browser's current window pathname is.
+	// const isCurrent = url.origin === env.NEXT_PUBLIC_APP_BASE_URL && url.pathname === pathname;
+
+	// fixed version which works for most cases but is a bit too eager (root path is always current)
+	let isCurrent = url.origin === env.NEXT_PUBLIC_APP_BASE_URL && pathname.startsWith(url.pathname);
+
+	// it's current if the href searchParams are a subset of the current pathname + search params
+	for (const [key, value] of url.searchParams) {
+		if (!searchParams.has(key) || searchParams.get(key) !== value) {
+			isCurrent = false;
+			break;
+		}
+	}
 
 	return {
 		"aria-current": isCurrent ? "page" : undefined,
