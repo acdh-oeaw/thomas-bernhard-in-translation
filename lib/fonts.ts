acdh-oeaw/template-app-next

feat: add app skeleton

lib/fonts.ts
@@ -1,0 +1,15 @@
+import { Inter } from "next/font/google";
+
+export const body = Inter({
+	axes: ["slnt"],
+	display: "swap",
+	subsets: ["latin", "latin-ext"],
+	variable: "--font-body",
+});
+
+export const heading = Inter({
+	axes: ["slnt"],
+	display: "swap",
+	subsets: ["latin", "latin-ext"],
+	variable: "--font-heading",
+});

acdh-oeaw/thomas-bernhard-in-translation

lib/fonts.ts
@@ -1,0 +1,17 @@
+import { Inter } from "next/font/google";
+
+export const body = Inter({
+	axes: ["opsz"],
+	display: "swap",
+	style: ["normal", "italic"],
+	subsets: ["latin", "latin-ext"],
+	variable: "--font-body",
+});
+
+export const heading = Inter({
+	axes: ["opsz"],
+	display: "swap",
+	style: ["normal", "italic"],
+	subsets: ["latin", "latin-ext"],
+	variable: "--font-heading",
+});