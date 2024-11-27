f71ed9f14f274d813d12908095395d243790955a
Kevin Stadler <kevin.stadler@oeaw.ac.at>
2024-11-27 15:01:08 +0100

    improvement: use client component-capable next image wrapper

--- components/image.tsx
+++ components/image.tsx
@@ -1,32 +1,18 @@
 /* eslint-disable no-restricted-imports */
 /* eslint-disable @next/next/no-img-element */
 
-import NextImage, { type ImageProps as NextImageProps } from "next/image";
-
-import { getImageDimensions } from "@/lib/get-image-dimensions";
+import NextImage, { getImageProps, type ImageProps as NextImageProps } from "next/image";
+import type { ReactNode } from "react";
 
 interface ImageProps extends Omit<NextImageProps, "loader"> {}
 
-export async function Image(props: ImageProps) {
-	const { alt = "", decoding = "async", fill, height, loading = "lazy", src, width } = props;
+export function Image(props: Readonly<ImageProps>): ReactNode {
+	const { alt = "", placeholder } = props;
 
-	const dimensions =
-		typeof src === "object" || fill === true || (width != null && height != null)
-			? { width, height }
-			: await getImageDimensions(src);
-
-	if (dimensions == null) {
-		return <img {...props} alt={alt} decoding={decoding} loading={loading} src={src as string} />;
+	/** @see https://nextjs.org/docs/pages/api-reference/components/image#getimageprops */
+	if (placeholder == null || placeholder === "empty") {
+		return <img {...getImageProps(props).props} alt={alt} />;
 	}
 
-	return (
-		<NextImage
-			{...props}
-			alt={alt}
-			decoding={decoding}
-			height={dimensions.height}
-			loading={loading}
-			width={dimensions.width}
-		/>
-	);
+	return <NextImage {...props} alt={alt} />;
 }
