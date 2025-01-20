cebd3e38d2dc1bc22b8819b673e7ebfd5c41d720
Kevin Stadler <kevin.stadler@oeaw.ac.at>
2025-01-20 09:13:00 +0100

    fix: correctly render works page refinement list on direct (SSR) page load
    Using the async (server) page components inside the InstantSearchProvider (from layout.tsx) led to
    the async loaded prop ('works') not being passed correctly when the page was directly navigated to from
    outside (it worked fine when navigating to/within the works page after the first website load, so
    this was a hydration/suspense boundary issue)

--- app/[locale]/works/[category]/[id]/WorksPage.tsx
+++ app/[locale]/works/[category]/[id]/WorksPage.tsx
@@ -6,6 +6,7 @@ import * as v from "valibot";
 import { Results } from "@/components/instantsearch/results";
 import { SingleRefinementDropdown } from "@/components/instantsearch/single-refinement-dropdown";
 import { SingleRefinementList } from "@/components/instantsearch/single-refinement-list";
+import { ThomasBernhardInstantSearchProvider } from "@/components/instantsearch/thomas-bernhard/thomasbernhard-instantsearchprovider";
 import { MainContent } from "@/components/main-content";
 import { type BernhardWork, type Category, otherCategories, proseCategories } from "@/lib/model";
 
@@ -57,40 +58,47 @@ export function WorksPage(props: WorksPageProps) {
 	};
 
 	return (
-		<MainContent>
-			<div className="grid h-full grid-cols-[25%_75%] gap-6 px-2">
-				<div className="relative h-full">
-					<SingleRefinementList
-						allLabel={categoryLabel}
-						attribute={"contains.work.id"}
-						// pathname={`/works/${category}`}
-						// format as title (year) instead of showing facet count
-						refinementArgs={{
-							transformItems,
-						}}
-					/>
+		<ThomasBernhardInstantSearchProvider
+			filters={`contains.work.category:=${category}`}
+			pageName={`works/${category}`}
+			pathnameField="contains.work.id"
+			queryArgsToMenuFields={{ language: "language" }}
+		>
+			<MainContent>
+				<div className="grid h-full grid-cols-[25%_75%] gap-6 px-2">
+					<div className="relative h-full">
+						<SingleRefinementList
+							allLabel={categoryLabel}
+							attribute={"contains.work.id"}
+							// pathname={`/works/${category}`}
+							// format as title (year) instead of showing facet count
+							refinementArgs={{
+								transformItems,
+							}}
+						/>
+					</div>
+					<Results className="h-full overflow-y-auto">
+						<SingleRefinementDropdown
+							allLabel={`${t("all")} ${t("filter_by.language")}`}
+							attribute={"language"}
+							className="min-w-40"
+							refinementArgs={{
+								transformItems: (items: Array<RefinementListItem>) => {
+									return items
+										.map((item) => {
+											// eslint-disable-next-line @typescript-eslint/no-explicit-any
+											item.label = `${tl(item.label as any).toLowerCase()} (${item.count.toString()})`;
+											return item;
+										})
+										.sort((a, b) => {
+											return a.label.localeCompare(b.label);
+										});
+								},
+							}}
+						/>
+					</Results>
 				</div>
-				<Results className="h-full overflow-y-auto">
-					<SingleRefinementDropdown
-						allLabel={`${t("all")} ${t("filter_by.language")}`}
-						attribute={"language"}
-						className="min-w-40"
-						refinementArgs={{
-							transformItems: (items: Array<RefinementListItem>) => {
-								return items
-									.map((item) => {
-										// eslint-disable-next-line @typescript-eslint/no-explicit-any
-										item.label = `${tl(item.label as any).toLowerCase()} (${item.count.toString()})`;
-										return item;
-									})
-									.sort((a, b) => {
-										return a.label.localeCompare(b.label);
-									});
-							},
-						}}
-					/>
-				</Results>
-			</div>
-		</MainContent>
+			</MainContent>
+		</ThomasBernhardInstantSearchProvider>
 	);
 }
