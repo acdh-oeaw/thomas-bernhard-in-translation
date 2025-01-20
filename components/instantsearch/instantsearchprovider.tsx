1184f75b98dab78ec6cfa848a64646631249bf2a
Kevin Stadler <kevin.stadler@oeaw.ac.at>
2025-01-20 10:13:00 +0100

    refactor: move instantsearch url config to dedicated lib/util functions

--- components/instantsearch/instantsearchprovider.tsx
+++ components/instantsearch/instantsearchprovider.tsx
@@ -1,21 +1,24 @@
 "use client";
 
-import type { UiState } from "instantsearch.js";
 import type { ReactNode } from "react";
 import { InstantSearchNext } from "react-instantsearch-nextjs";
 import type { SearchClient } from "typesense-instantsearch-adapter";
 
+import {
+	instantSearchNextAbbreviatedStateMapping,
+	instantSearchNextRefinementInPathnameRouterConfig,
+} from "@/lib/utils/instantsearch-urls";
+
 export interface InstantSearchProviderProps {
 	children?: ReactNode;
+	// TODO refactor by referring to arguments of the config functions below
+	searchClient: SearchClient;
 	collectionName: string;
-	pageName?: string;
-	pathnameField?: string;
 	queryArgsToMenuFields?: Record<string, string>;
 	defaultSort?: string;
-	searchClient: SearchClient;
+	pageName?: string;
+	pathnameField?: string;
 }
-
-type RouteState = Record<string, string | undefined>;
 
 export function InstantSearchProvider(props: InstantSearchProviderProps): ReactNode {
 	const {
@@ -32,105 +35,13 @@ export function InstantSearchProvider(props: InstantSearchProviderProps): ReactN
 		<InstantSearchNext
 			indexName={collectionName}
 			routing={{
-				router: {
-					// "The difference here is that routing.router takes the same options as the historyRouter."
-					createURL({ location, routeState, qsModule }) {
-						const parts = location.pathname.split("/");
-						if (pageName) {
-							const splitPageName = pageName.split("/");
-							if (parts.at(-1) !== splitPageName.at(-1)) {
-								parts.pop();
-							}
-							// don't add value if it is undefined
-							if (pathnameField && routeState[pathnameField]) {
-								parts.push(routeState[pathnameField]);
-
-								// remove field from state so it isn't also added to queryargs
-								// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-								delete routeState[pathnameField];
-							}
-						}
-						const qa = qsModule.stringify(routeState);
-						return location.origin + parts.join("/") + (qa && `?${qa}`);
-					},
-					parseURL({ qsModule, location }) {
-						const queryArgs = qsModule.parse(location.search.slice(1));
-
-						if (queryArgs.query) {
-							queryArgs.query = decodeURIComponent(queryArgs.query as string);
-						}
-
-						if (pageName) {
-							// trim leading and trailing slashes
-							const parts = location.pathname.replace(/^\/+|\/+$/gm, "").split("/");
-							const splitPageName = pageName.split("/");
-							if (parts.at(-1) !== splitPageName.at(-1)) {
-								// the last element is not the expected page name, assume it's the value of the
-								// pathnameField
-								queryArgs[pathnameField ?? pageName] = parts.at(-1);
-							}
-						}
-						return queryArgs as RouteState;
-					},
-				},
-				stateMapping: {
-					routeToState(routeState: RouteState) {
-						if (!("sort" in routeState)) {
-							routeState.sort = defaultSort;
-						}
-						const uiState = {
-							[collectionName]: {
-								query: routeState.q && decodeURI(routeState.q),
-								menu: {},
-								// refinementList: {},
-								sortBy: routeState.sort && `${collectionName}/sort/${routeState.sort}`,
-							},
-						} as UiState;
-						if (queryArgsToMenuFields) {
-							Object.entries(queryArgsToMenuFields).forEach(([queryArg, field]) => {
-								if (routeState[queryArg]) {
-									uiState[collectionName]!.menu![field] = decodeURI(routeState[queryArg]);
-								}
-							});
-						}
-						if (pathnameField && routeState[pathnameField]) {
-							uiState[collectionName]!.menu![pathnameField] = routeState[pathnameField];
-						}
-						return uiState;
-					},
-					stateToRoute: (uiState: UiState) => {
-						const indexUiState = uiState[collectionName]!;
-						const route = {} as RouteState;
-						if (pathnameField) {
-							route[pathnameField] = undefined;
-						}
-						if (indexUiState.query) {
-							route.q = encodeURI(indexUiState.query);
-						}
-						if (indexUiState.sortBy) {
-							const sortBy = indexUiState.sortBy.split("/").at(-1);
-							if (sortBy !== defaultSort) {
-								route.sort = sortBy;
-							}
-						}
-						if (indexUiState.menu) {
-							if (queryArgsToMenuFields) {
-								for (const [field, value] of Object.entries(indexUiState.menu)) {
-									const queryarg = Object.entries(queryArgsToMenuFields).find(([_k, v]) => {
-										return v === field;
-									})?.[0];
-									if (queryarg) {
-										route[queryarg] = encodeURI(value);
-									}
-								}
-							}
-							if (pathnameField) {
-								route[pathnameField] = indexUiState.menu[pathnameField];
-							}
-						}
-						return route;
-					},
-				},
+				router: instantSearchNextRefinementInPathnameRouterConfig(pageName, pathnameField),
+				stateMapping: instantSearchNextAbbreviatedStateMapping(
+					collectionName,
+					defaultSort,
+					queryArgsToMenuFields,
+					pathnameField,
+				),
 			}}
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 			searchClient={searchClient}
---
+++ lib/utils/instantsearch-urls.ts
@@ -0,0 +1,117 @@
+import type { StateMapping, UiState } from "instantsearch.js";
+import type { BrowserHistoryArgs } from "instantsearch.js/es/lib/routers/history";
+
+type RouteState = Record<string, string | undefined>;
+
+// "The difference here is that routing.router takes the same options as the historyRouter."
+export function instantSearchNextRefinementInPathnameRouterConfig(
+	pageName?: string,
+	pathnameField?: string,
+): Partial<BrowserHistoryArgs<RouteState>> {
+	return {
+		createURL({ location, routeState, qsModule }) {
+			const parts = location.pathname.split("/");
+			if (pageName) {
+				const splitPageName = pageName.split("/");
+				if (parts.at(-1) !== splitPageName.at(-1)) {
+					parts.pop();
+				}
+				// don't add value if it is undefined
+				if (pathnameField && routeState[pathnameField]) {
+					parts.push(routeState[pathnameField]);
+
+					// remove field from state so it isn't also added to queryargs
+					// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+					delete routeState[pathnameField];
+				}
+			}
+			const qa = qsModule.stringify(routeState);
+			return location.origin + parts.join("/") + (qa && `?${qa}`);
+		},
+		parseURL({ qsModule, location }) {
+			const queryArgs = qsModule.parse(location.search.slice(1));
+
+			if (queryArgs.query) {
+				queryArgs.query = decodeURIComponent(queryArgs.query as string);
+			}
+
+			if (pageName) {
+				// trim leading and trailing slashes
+				const parts = location.pathname.replace(/^\/+|\/+$/gm, "").split("/");
+				const splitPageName = pageName.split("/");
+				if (parts.at(-1) !== splitPageName.at(-1)) {
+					// the last element is not the expected page name, assume it's the value of the
+					// pathnameField
+					queryArgs[pathnameField ?? pageName] = parts.at(-1);
+				}
+			}
+			return queryArgs as RouteState;
+		},
+	};
+}
+
+export function instantSearchNextAbbreviatedStateMapping(
+	collectionName: string,
+	defaultSort?: string,
+	queryArgsToMenuFields?: Record<string, string>,
+	pathnameField?: string,
+): StateMapping<UiState, RouteState> {
+	return {
+		routeToState(routeToState: RouteState) {
+			if (!("sort" in routeToState)) {
+				routeToState.sort = defaultSort;
+			}
+			const uiState = {
+				[collectionName]: {
+					query: routeToState.q && decodeURI(routeToState.q),
+					menu: {},
+					// refinementList: {},
+					sortBy: routeToState.sort && `${collectionName}/sort/${routeToState.sort}`,
+				},
+			} as UiState;
+			if (queryArgsToMenuFields) {
+				Object.entries(queryArgsToMenuFields).forEach(([queryArg, field]) => {
+					if (routeToState[queryArg]) {
+						uiState[collectionName]!.menu![field] = decodeURI(routeToState[queryArg]);
+					}
+				});
+			}
+			if (pathnameField && routeToState[pathnameField]) {
+				uiState[collectionName]!.menu![pathnameField] = routeToState[pathnameField];
+			}
+			return uiState;
+		},
+		stateToRoute: (uiState: UiState) => {
+			const indexUiState = uiState[collectionName]!;
+			const route = {} as RouteState;
+			if (pathnameField) {
+				route[pathnameField] = undefined;
+			}
+			if (indexUiState.query) {
+				route.q = encodeURI(indexUiState.query);
+			}
+			if (indexUiState.sortBy) {
+				const sortBy = indexUiState.sortBy.split("/").at(-1);
+				if (sortBy !== defaultSort) {
+					route.sort = sortBy;
+				}
+			}
+			if (indexUiState.menu) {
+				if (queryArgsToMenuFields) {
+					for (const [field, value] of Object.entries(indexUiState.menu)) {
+						const queryarg = Object.entries(queryArgsToMenuFields).find(([_k, v]) => {
+							return v === field;
+						})?.[0];
+						if (queryarg) {
+							route[queryarg] = encodeURI(value);
+						}
+					}
+				}
+				if (pathnameField) {
+					route[pathnameField] = indexUiState.menu[pathnameField];
+				}
+			}
+			return route;
+		},
+	};
+}
