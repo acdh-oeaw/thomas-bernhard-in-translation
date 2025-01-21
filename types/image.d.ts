0c7daf0855be2d7ecc9c0935d6eb1476b5d66920
Kevin Stadler <kevin.stadler@oeaw.ac.at>
2025-01-21 21:33:38 +0100

    fix: declare png image type to satisfy linter

--- types/image.d.ts
+++ types/image.d.ts
@@ -1,3 +1,12 @@
+declare module "*.png" {
+	// eslint-disable-next-line no-restricted-imports
+	import type { StaticImageData } from "next/image";
+
+	const content: StaticImageData;
+
+	export default content;
+}
+
 declare module "*.svg" {
 	// eslint-disable-next-line no-restricted-imports
 	import type { StaticImageData } from "next/image";
