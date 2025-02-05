eb5cdf6e13a282021590974cd08a236791a70021
Kevin Stadler <kevin.stadler@oeaw.ac.at>
2025-02-05 15:15:36 +0100

    improvement: make about page logos into links

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -9,6 +9,7 @@ import { read } from "to-vfile";
 import { unified } from "unified";
 
 import { Image } from "@/components/image";
+import { Link } from "@/components/link";
 import { MainContent } from "@/components/main-content";
 import type { Locale } from "@/config/i18n.config";
 import forschungsstelle from "@/public/assets/images/forschungsstelle.png";
@@ -39,22 +40,34 @@ export default async function AboutPage(props: AboutPageProps) {
 				<section dangerouslySetInnerHTML={{ __html: String(result) }} className="prose" />
 				<div className="flex flex-row flex-wrap items-center justify-center">
 					<div className="mt-3 flex flex-col items-center gap-3">
-						<Image
-							alt="Suhrkamp Verlag Logo"
-							className="w-44 max-w-full dark:invert"
-							src={suhrkamp}
-						/>
-						<Image
-							alt="Internationale Thomas Bernhard Gesellschaft Logo"
-							className="w-80 max-w-full dark:invert"
-							src={itbg}
-						/>
+						<Link href="https://www.suhrkamp.de/" target="_blank">
+							<Image
+								alt="Suhrkamp Verlag Logo"
+								className="w-44 max-w-full dark:invert"
+								src={suhrkamp}
+							/>
+						</Link>
+						<Link
+							href="https://thomasbernhard.at/internationale-thomas-bernhard-gesellschaft/"
+							target="_blank"
+						>
+							<Image
+								alt="Internationale Thomas Bernhard Gesellschaft Logo"
+								className="w-80 max-w-full dark:invert"
+								src={itbg}
+							/>
+						</Link>
 					</div>
-					<Image
-						alt="Forschungsstelle Thomas Bernhard Logo"
-						className="w-52 max-w-full dark:invert"
-						src={forschungsstelle}
-					/>
+					<Link
+						href="https://www.oeaw.ac.at/acdh/research/literary-textual-studies/research/authors-editions/ftb-thomas-bernhard-research-centre"
+						target="_blank"
+					>
+						<Image
+							alt="Forschungsstelle Thomas Bernhard Logo"
+							className="w-52 max-w-full dark:invert"
+							src={forschungsstelle}
+						/>
+					</Link>
 				</div>
 			</MainContent>
 		);
